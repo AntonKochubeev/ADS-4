1536997c4915829e0fc996c2dbd9f12db8e417a0
AntonKochubeev <160320922+AntonKochubeev@users.noreply.github.com>
2024-03-24 21:18:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,26 @@
+int findFirstGreaterThan(int *arr, int size, int value) {
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] > value) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+int binarySearch(int *arr, int left, int right, int value) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            return mid;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
@@ -28,20 +51,6 @@ int countPairs2(int *arr, int len, int value) {
     }
 
     return count;
-}
-
-int binarySearch(int *arr, int left, int right, int value) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == value) {
-            return mid;
-        } else if (arr[mid] < value) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
 }
 
 int countPairs3(int *arr, int len, int value) {
