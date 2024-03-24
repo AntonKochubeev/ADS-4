732009d460619576b6bb7863db0d4c9f1f37aadd
AntonKochubeev <160320922+AntonKochubeev@users.noreply.github.com>
2024-03-24 21:22:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,57 +7,65 @@ int findFirstGreaterThan(int *arr, int size, int value) {
     return 0;
 }
 
-int binarySearch(int *arr, int left, int right, int value) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == value) {
-            return mid;
-        } else if (arr[mid] < value) {
-            left = mid + 1;
+int binarySearch(int *arr, int size, int value) {
+    int found = 0;
+    int first = 0;
+    int last = size - 1;
+    int middle = 0;
+    while (first <= last) {
+        middle = (first + last) / 2;
+        if (arr[middle] == value) {
+            ++found;
+            int index = middle - 1;
+            while (index >= first && arr[index] == value) {
+                ++found;
+                --index;
+            }
+            index = middle + 1;
+            while (index <= last && arr[index] == value) {
+                ++found;
+                ++index;
+            }
+            return found;
+        } else if (arr[middle] < value) {
+            first = middle + 1;
         } else {
-            right = mid - 1;
+            last = middle - 1;
         }
     }
-    return -1;
+    return 0;
 }
 
 int countPairs1(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                count++;
+    int result = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (value == arr[i] + arr[j]) {
+                ++result;
             }
         }
     }
-    return count;
+    return result;
 }
 
 int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    int left = 0;
-    int right = len - 1;
-
-    while (left < right) {
-        if (arr[left] + arr[right] == value) {
-            count++;
-            left++;
-            right--;
-        } else if (arr[left] + arr[right] < value) {
-            left++;
-        } else {
-            right--;
+    int result = 0;
+    len = findFirstGreaterThan(arr, len, value);
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (value == arr[i] + arr[j]) {
+                ++result;
+            }
         }
     }
-
-    return count;
+    return result;
 }
 
 int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        int secondValue = value - arr[i];
-        count += binarySearch(arr + i + 1, len - i - 1, n);
+    int result = 0;
+    for (int i = 0; i < len; ++i) {
+        int n = value - arr[i];
+        result += binarySearch(arr + i + 1, len - i - 1, n);
     }
-    return count;
+    return result;
 }
